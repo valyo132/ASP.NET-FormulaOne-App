6e31a6515676ec65b2fe69d13900e96848b27be7
Valentin Markov <101991279+valyo132@users.noreply.github.com>
2023-12-16 19:47:00 +0200

    Added Admin role and admin user

--- FormulaOneApp/FormulaOneApp.Web/Controllers/HomeController.cs
+++ FormulaOneApp/FormulaOneApp.Web/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace FormulaOneApp.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
             return View();
         }
